17b63ccc280513beebaac535d64260a5051f90f0
ManviRankawat <manviranka@gmail.com>
2025-02-17 22:09:26 -0500

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import LoginSignup from './Components/LoginSignupPage/LoginSignup';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Task Management App</h1>
+        <LoginSignup/>
+    </div>
+  );
+    
+}
+
+export default App;
